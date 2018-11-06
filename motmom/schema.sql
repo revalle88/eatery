4e05776f5a1c4a965fd0ba2c69d6105cdb901ce4
revalle <revalle88@gmail.com>
2018-11-06 21:52:03 +0300

    initialize db schema

--- motmom/schema.sql
+++ motmom/schema.sql
@@ -1,19 +1,41 @@
-/*CREATE TABLE products(
+CREATE TABLE products(
     id        serial PRIMARY KEY,
     name       varchar(100) NOT NULL,
-    description        varchar(200)
+    description        varchar(200),
+    price        integer NOT NULL
 );
+ALTER TABLE products
+  OWNER TO postgres;
 
-CREATE TABLE public.products
-(
-  id integer NOT NULL DEFAULT nextval('products_id_seq'::regclass),
-  name character varying(100) NOT NULL,
-  description character varying(200),
-  CONSTRAINT products_pkey PRIMARY KEY (id)
-)
+CREATE TABLE users(
+  id        serial PRIMARY KEY,
+  username       varchar(100) NOT NULL,
+  password       varchar(100) NOT NULL,
+  role       varchar(100) NOT NULL
+);
+ALTER TABLE users
+  OWNER TO postgres;
 
-insert into products (id, name, description) values (0, 'Bread', 'descripiton');
-insert into products (id, name, description) values (1, 'Butter', 'descri2');
-insert into products (id, name, description) values (2, 'Cookie', 'descr3');
+CREATE TABLE bids(
+  id        serial PRIMARY KEY,
+  user_id        integer NOT NULL,
+  food        varchar(200),
+  price        integer NOT NULL,
+  qrcode       varchar(100),
+  comment       varchar(100)
+);
+ALTER TABLE bids
+  OWNER TO postgres;
 
-	*/
+
+
+insert into products (id, name, description, price) values (0, 'Суп Гороховый', 'descripiton', 23);
+insert into products (id, name, description, price) values (1, 'Картофель', 'descripiton', 32);
+insert into products (id, name, description, price) values (2, 'Хлеб', 'descripiton', 7);
+insert into products (id, name, description, price) values (3, 'Чай', 'descripiton', 12);
+insert into products (id, name, description, price) values (4, 'Компот', 'descripiton', 15);
+
+/*
+insert into users (id, username, password, role) values (0, 'baker', 'baker', 'baker');
+insert into users (id, username, password, role) values (1, 'developer', 'developer', 'developer');
+insert into users (id, username, password, role) values (2, 'hello', 'world', 'developer');*/
--- motmom/subscribers.py
+++ motmom/subscribers.py
@@ -18,7 +18,7 @@ def new_request_subscriber(event):
     request = event.request
     settings = request.registry.settings
     # TODO убрать настройки в файл конфигурации
-    request.conn = psycopg2.connect("dbname=motmom user=postgres password=gliA6kkvn host=localhost")
+    request.conn = psycopg2.connect("dbname=motmom1 user=postgres password=gliA6kkvn host=localhost")
     request.cur = request.conn.cursor()
     request.add_finished_callback(close_db_connection)
 
@@ -34,8 +34,8 @@ def application_created_subscriber(event):
     with open(os.path.join(here, 'schema.sql')) as f:
         stmt = f.read()
         # TODO убрать настройки в файл конфигурации
-        conn = psycopg2.connect("dbname=motmom user=postgres password=gliA6kkvn host=localhost")
+        conn = psycopg2.connect("dbname=motmom1 user=postgres password=gliA6kkvn host=localhost")
         cur = conn.cursor()
-        # cur.execute(stmt)
-        # conn.commit()
+        cur.execute(stmt)
+        conn.commit()
         log.warning('statement executed...')
