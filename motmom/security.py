567464bdb8af2f3f28371fab699998f675f7d6d1
revalle <revalle88@gmail.com>
2018-11-09 00:01:50 +0300

    quotes refactor

--- motmom/security.py
+++ motmom/security.py
@@ -1,7 +1,7 @@
 def groupfinder(user_id, request):
     user_role = ''
     if user_id:
-        request.cur.execute("select role from users where id = %s ",
+        request.cur.execute('select role from users where id = %s ',
                             (user_id,))
         user_role = request.cur.fetchone()[0]
         return [user_role]
@@ -12,7 +12,7 @@ def groupfinder(user_id, request):
 def get_user_role(request):
     user_id = request.authenticated_userid
     if user_id is not None:
-        request.cur.execute("select role from users where id = %s ",
+        request.cur.execute('select role from users where id = %s ',
                             (user_id,))
         user_role = request.cur.fetchone()[0]
         return user_role
@@ -22,7 +22,7 @@ def get_user_role(request):
 def get_user_name(request):
     user_id = request.authenticated_userid
     if user_id is not None:
-        request.cur.execute("select username from users where id = %s ",
+        request.cur.execute('select username from users where id = %s ',
                             (user_id,))
         user_name = request.cur.fetchone()[0]
         return user_name
